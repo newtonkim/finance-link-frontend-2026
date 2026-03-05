943c5aaef38e788bac7ec0282f6ebc6b2925c65c
sengendo Nazil <ssengendonazil@gmail>
2026-03-05 13:54:10 +0300

    centarlize the  componets

--- src/Global/AppContent.vue
+++ src/Global/AppContent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { SidebarInset } from '@/Global/ui/sidebar';
+import { SidebarInset } from '@/Global';
 
 type Props = {
     variant?: 'header' | 'sidebar';
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { setBaseURL } from "septor-store";
 const app = createApp(App)
-setBaseURL({ backendURL: import.meta.env.VITE_BACKEND_URL });
+// setBaseURL({ backendURL: import.meta.env.VITE_BACKEND_URL });
 app.use(createPinia())
 app.use(router)
 
