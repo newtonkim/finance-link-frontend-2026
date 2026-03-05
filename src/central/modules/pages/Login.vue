b468b7d480c0b1f21c125fcc3f30f6898abd3399
newtonkim <knewts98@gmail.com>
2026-03-05 09:19:53 +0300

    remove white space

--- src/central/modules/pages/Login.vue
+++ src/central/modules/pages/Login.vue
@@ -14,6 +14,7 @@ import { Spinner } from '@/components/ui/spinner';
 import AuthBase from '@/layouts/auth/AuthSplitLayout.vue';
 
 const router = useRouter();
+j 6fbd5 bv
 
 const email = ref('');
 const password = ref('');
