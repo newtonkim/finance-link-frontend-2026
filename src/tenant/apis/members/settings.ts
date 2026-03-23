1e79035713184720b7a4dbf649b9910e0b04441b
Nazil's imac <nazils@Nazilss-iMac.local>
2026-03-23 10:33:16 +0300

    Update savings account services and permissions

--- src/tenant/apis/members/settings.ts
+++ src/tenant/apis/members/settings.ts
@@ -1,25 +1,7 @@
 import { fetchTableData, formDataFormat, scopeValues } from '@/Global';
-import { notify } from '@/Global/Toasters';
 import { pomPinia } from 'septor-store';
-
 export function memmberSettingApi() {
     const Store = pomPinia();
-    function feedback(collection:any,success:string,fail:string) {
-             const res: any = Store.stateGenaratorApi(collection);
-        let msg: Record<string, string> = {
-            msg:success ,
-            type: 'Error',
-        };
-        if (!res || res.status == 200) {
-            msg = {
-                msg: fail,
-                type: 'Success',
-            };
-        }
-        notify(msg);
-        return res
-    }
-
     function settingsList(data?: Object) {
     fetchTableData({
         data: data,
@@ -29,7 +11,6 @@ export function memmberSettingApi() {
       })
  
     }
-
     return {
         settingsList
     };
