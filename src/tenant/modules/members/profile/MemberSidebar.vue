8b7930209fc68882b74408a9a700995d7764ed5d
sengendo Nazil <ssengendonazil@gmail>
2026-04-10 11:57:17 +0300

    fixing role addtion

--- src/tenant/modules/members/profile/MemberSidebar.vue
+++ src/tenant/modules/members/profile/MemberSidebar.vue
@@ -53,6 +53,7 @@ const formatCleanDate = (date) => {
 }
 </script>
 <template>
+    
     <div class="w-full lg:w-[280px] shrink-0 flex flex-col gap-6">
         <!-- Member Card -->
         <div class="bg-white border border-gray-100 rounded-2xl overflow-hidden shadow-sm">
