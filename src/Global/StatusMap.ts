f22591827e7481debe3fe0f82fa755c8a2dea5b2
sengendo Nazil <ssengendonazil@gmail>
2026-04-15 11:10:31 +0300

    add more statuses in the map

--- src/Global/StatusMap.ts
+++ src/Global/StatusMap.ts
@@ -1,110 +1,110 @@
 export const status = {
   pending: {
     label: 'Pending',
-    className: ' text-xs font-bold px-2 py-1 rounded-full   bg-yellow-100 text-yellow-700',
+    className: ' text-sm font-medium px-3 text-sm py-1 rounded-full   bg-yellow-100 text-yellow-700',
   },
   active: {
     label: 'Active',
-    className: ' text-xs font-bold px-2 py-1 rounded-full   bg-green-100 text-green-700',
+    className: ' text-sm     bg-green-100 text-green-700   dark:text-emerald-400 inline-flex items-center px-3 py-0 text-sm font-medium rounded-full',
   },
   expired: {
     label: 'expired',
     className:
-      ' text-xs font-bold px-2 py-1 rounded-full bg-rose-50 text-rose-700 dark:bg-rose-500/10 dark:text-rose-400',
+      ' text-sm font-medium px-3 text-sm py-1 rounded-full bg-rose-50 text-rose-700 dark:bg-rose-500/10 dark:text-rose-400',
   },
   trial: {
     label: 'transit',
     className:
-      ' text-xs font-bold px-2 py-1 rounded-full bg-amber-50 text-amber-700 dark:bg-amber-500/10 dark:text-amber-400',
+      ' text-sm font-medium px-3 text-sm py-1 rounded-full bg-amber-50 text-amber-700 dark:bg-amber-500/10 dark:text-amber-400',
   },
   basic: {
     label: 'basic',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-white text-amber-700 shadow-sm',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-white text-amber-700 shadow-sm',
   },
   enterprise: {
     label: 'enterprise',
     className:
-      'text-xs font-bold px-2 py-1 rounded-full bg-amber-500/10 text-neutral-700 shadow-sm',
+      'text-sm font-medium px-3 text-sm py-1 rounded-full bg-amber-500/10 text-neutral-700 shadow-sm',
   },
   professional: {
     label: 'Pending Approval',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-yellow-100 text-yellow-700 shadow-sm',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-yellow-100 text-yellow-700 shadow-sm',
   },
   suspended: {
     label: 'Suspended',
     className:
-      ' text-xs font-bold px-2 py-1 rounded-fullbg-orange-50 text-orange-700 dark:bg-orange-500/10 dark:text-orange-400   ',
+      ' text-sm font-medium px-3 text-sm py-1 rounded-fullbg-orange-50 text-orange-700 dark:bg-orange-500/10 dark:text-orange-400   ',
   },
   fired: {
     label: 'Fired',
-    className: ' text-xs font-bold px-2 py-1 rounded-full bg-red-100 text-red-700',
+    className: ' text-sm font-medium px-3 text-sm py-1 rounded-full bg-red-100 text-red-700',
   },
   failed: {
     label: 'Failed',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-red-100 text-red-600',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-red-100 text-red-600',
   },
 }
 
 export const paymentStatus = {
   unpaid: {
     label: 'Unpaid',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-red-100 text-red-700',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-red-100 text-red-700',
   },
   paid: {
     label: 'Paid',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-green-100 text-green-700',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-green-100 text-green-700',
   },
   completed: {
     label: 'completed',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-green-200 text-green-700',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-green-200 text-green-700',
   },
   partially_paid: {
     label: 'Partially Paid',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-yellow-100 text-yellow-700',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-yellow-100 text-yellow-700',
   },
   refunded: {
     label: 'Refunded',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-blue-100 text-blue-700',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-blue-100 text-blue-700',
   },
   failed_payment: {
     label: 'Failed Payment',
-    className: 'text-xs font-bold px-2 py-1 rounded-full bg-red-100 text-red-600',
+    className: 'text-sm font-medium px-3 text-sm py-1 rounded-full bg-red-100 text-red-600',
   },
 }
 
 export const paymentMethods = {
-  cash: { label: 'Cash', className: ' font-bold px-2 py-1 rounded-full     text-green-700' },
-  card: { label: 'Card', className: '  font-bold px-2 py-1 rounded-full  text-blue-600' },
-  bank: { label: 'Bank', className: '  font-bold px-2 py-1 rounded-full  text-blue-600' },
+  cash: { label: 'Cash', className: ' font-medium px-3 text-sm py-1 rounded-full     text-green-700' },
+  card: { label: 'Card', className: '  font-medium px-3 text-sm py-1 rounded-full  text-blue-600' },
+  bank: { label: 'Bank', className: '  font-medium px-3 text-sm py-1 rounded-full  text-blue-600' },
   mobile_money: {
     label: 'Mobile Money',
-    className: ' font-bold px-0 py-1 rounded-full  text-blue-700',
+    className: ' font-medium px-0 py-1 rounded-full  text-blue-700',
   },
 }
 export const systemRoles = {
   admin: {
     label: 'admin',
-    className: 'font-bold px-2 py-1 rounded-full text-purple-700',
+    className: 'font-medium px-3 text-sm py-1 rounded-full text-purple-700',
   },
 
   'super-admin': {
     label: 'supper admin',
-    className: 'font-bold px-2 py-1 rounded-full text-neutral-600/60',
+    className: 'font-medium px-3 text-sm py-1 rounded-full text-neutral-600/60',
   },
 
   ordinary: {
     label: 'Ordinary',
-    className: 'font-bold px-2 py-1 rounded-full text-teal-600',
+    className: 'font-medium px-3 text-sm py-1 rounded-full text-teal-600',
   },
 
   manager: {
     label: 'Manager',
-    className: 'font-bold px-2 py-1 rounded-full text-red-600',
+    className: 'font-medium px-3 text-sm py-1 rounded-full text-red-600',
   },
 
   accountant: {
     label: 'Accountant',
-    className: 'font-bold px-2 py-1 rounded-full text-indigo-600',
+    className: 'font-medium px-3 text-sm py-1 rounded-full text-indigo-600',
   },
 }
 // maritalOptions: [{ id: 'single', name: 'Single' }, { id: 'married', name: 'Married' }, { id: 'divorced', name: 'Divorced' }, { id: 'widowed', name: 'Widowed' }]
